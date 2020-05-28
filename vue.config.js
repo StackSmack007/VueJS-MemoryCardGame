5d9b3af4347a302e8bce16c701464e3027c09a54
Nikolay <n_hristov007@abv.bg>
2020-05-29 00:58:58 +0300

    add

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-// module.exports = {
-//     publicPath: process.env.NODE_ENV === 'production'
-//       ? '/VueJS-Memory-Card-Game/'
-//       : '/'
-//   }
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/VueJS-MemoryCardGame/'
+      : '/'
+  }
